38f79e3e84a296df862d22bb1772233363f23b5b
schexi <victoria.skola@gmail.com>
2026-02-26 19:46:39 +0100

    Skapa utkast av Loans-API med CRUD

---
+++ Loans-API/Program.cs
@@ -0,0 +1,21 @@
+using LoansApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LoansContext>(options =>
+    options.UseSqlite("Data Source=loans.db"));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
