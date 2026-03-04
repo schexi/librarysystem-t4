72778adbe9df3b67ca25d519802290e04cd16bfa
momed98 <mohammedabdu0406@gmail.com>
2026-03-04 12:31:42 +0100

    Uppdatera ItemsController med CRUD och Swagger

--- Items-API/Program.cs
+++ Items-API/Program.cs
@@ -4,18 +4,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=items.db"));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
 
 app.Run();
