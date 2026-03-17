07b366a7e5c8349e0dd8e848bb3027ffe4e00398
schexi <victoria.skola@gmail.com>
2026-03-17 14:37:17 +0100

    Byt till Scalar från Swashbuckle/Swagger

--- Loans-API/Program.cs
+++ Loans-API/Program.cs
@@ -1,20 +1,32 @@
 using Loans_API.KeyFilters;
 using LoansApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://localhost:5100"); // Tvinga port 5100
+
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LoansContext>(options =>
-    options.UseSqlite("Data Source=loans.db"));
+builder.Services.AddOpenApi(); 
+builder.Services.AddDbContext<LoansContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ApiKeyFilter>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<LoansContext>();
+    dbContext.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
