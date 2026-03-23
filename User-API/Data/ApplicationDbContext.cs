d96b8e85668616e7f5b72b364ca92178af1b42fe
schexi <victoria.skola@gmail.com>
2026-03-23 16:39:48 +0100

    Deploy branch för Library Systems MVC

--- User-API/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Library.User.Api.Models.Entities;
-
-namespace Library.User.Api.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<UserEntity> Users { get; set; } = null!;
-}
