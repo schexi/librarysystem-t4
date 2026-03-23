384edeb3c0ee285712d261064bc45a50e6eda78a
Mahad <mahadahmed116@gmail.com>
2026-03-24 00:51:11 +0100

    Skapat ny kategori-api

---
+++ Kategori-Api/Models/Kategori.cs
@@ -0,0 +1,8 @@
+namespace Kategori_Api.Models;
+
+public class Kategori
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+}
