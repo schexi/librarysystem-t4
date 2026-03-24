471d80559d4fb4533493b5ca97dfbc8c6d8d8592
momed98 <mohammedabdu0406@gmail.com>
2026-03-24 20:34:18 +0100

    Bara Items-API från main

--- LibrarySystem-T4/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace LibrarySystem_T4.Models;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Description { get; set; } = "";
-}
