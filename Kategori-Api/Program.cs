53aac70c11988873564e8efdd7a8fc04ccc8f0eb
Mahad <mahadahmed116@gmail.com>
2026-03-27 00:51:37 +0100

    Fixa databas och HTTPS för Azure

--- Kategori-Api/Program.cs
+++ Kategori-Api/Program.cs
@@ -1,5 +1,5 @@
-using Scalar.AspNetCore;
-using Microsoft.EntityFrameworkCore; 
+/*using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using Kategori_Api.Data;
 
 
@@ -29,6 +29,37 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();*/
+
+using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore; 
+using Kategori_Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "categories.db");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+
 
 app.UseAuthorization();
 
