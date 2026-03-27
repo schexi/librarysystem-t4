31ad6850fda27b42dc50742ff07e0d21c698ca77
Mahad <mahadahmed116@gmail.com>
2026-03-27 11:05:11 +0100

    Rensa deploy-branch, behåll bara LibrarySystem-T4

--- Kategori-Api/Models/Kategori.cs
+++
@@ -1,8 +0,0 @@
-namespace Kategori_Api.Models;
-
-public class Kategori
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Description { get; set; } = "";
-}
