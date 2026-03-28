6a37601cce3ef7c27db49dc8a327e44819aeec74
Adishka0304 <Adishka.tarin@gmail.com>
2026-03-28 21:17:58 +0100

    Fix: ta bort bin/obj från repo och lägg till gitignore

--- publish/wwwroot/js/site.js
+++
@@ -1,7 +0,0 @@
-console.log("Site.js laddad - globala funktioner kan läggas här");
-
-// T.ex. global logout funktion
-async function logout() {
-    await fetch('/Authorization/Logout', { method: 'POST', credentials: 'include' });
-    window.location.href = '/Authorization/Login';
-}
