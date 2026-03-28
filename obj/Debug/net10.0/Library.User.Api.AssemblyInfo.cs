8c10c37c69d747d9ef07708f2394cbb865cca8f4
Adishka0304 <Adishka.tarin@gmail.com>
2026-03-28 02:47:09 +0100

    Koppla User-API login till navbar

--- obj/Debug/net10.0/Library.User.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Library.User.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.User.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2f74c21a40779d36f5b3ac8dd219506f4451e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca80ca12c69f8b9b7ae774024bf0c64b7671c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.User.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.User.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
