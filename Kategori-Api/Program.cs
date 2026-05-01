71dbfef57337c338cc5ce08249248baad8b2524e
Mahad <mahadahmed116@gmail.com>
2026-05-02 00:46:35 +0200

    Lägg till Kategori-Api-kod från deploy-branchen

--- Kategori-Api/Program.cs
+++ Kategori-Api/Program.cs
@@ -2,33 +2,40 @@ using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore; 
 using Kategori_Api.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Lägg till CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
-
-
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "categories.db");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=categories.db"));
-
-
+    options.UseSqlite($"Data Source={dbPath}"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
 }
 
-app.UseHttpsRedirection();
+// Använd CORS
+app.UseCors("AllowAll");
+
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+
 
 app.UseAuthorization();
 
